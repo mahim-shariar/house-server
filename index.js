42913f3855f85248928c3e267ad9c1725588f040
mahim-shariar <mdmahim924214@gmail.co>
2022-01-03 16:37:34 +0600

    mahim

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
+// mahim
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k4g9x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
